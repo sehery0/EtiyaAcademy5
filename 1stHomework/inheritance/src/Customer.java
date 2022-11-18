e304dce949c254db14f3427c1bdabdeef77e3e1f
Seher Yalçın <106523360+sehery0@users.noreply.github.com>
2022-11-18 16:34:46 +0300

    revised

---
+++ 1stHomework/inheritance/src/Customer.java
@@ -0,0 +1,7 @@
+public class Customer extends Person {
+    int id;
+    String firstName;
+    String lastName;
+    int age;
+    String email;
+}
---
+++ 1stHomework/inheritance/src/Employee.java
@@ -0,0 +1,8 @@
+public class Employee extends Person{
+    int id;
+    String firstName;
+    String lastName;
+    int age;
+    double salary;
+
+}
